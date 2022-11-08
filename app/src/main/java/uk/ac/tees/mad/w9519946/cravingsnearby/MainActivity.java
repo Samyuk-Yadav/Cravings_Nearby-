c818a86951e34037f97d19a1b405a5d786c7dcbc
Samyuk Yadav Banda <aronisam325@gmail.com>
2022-11-08 11:25:58 +0000

    Animations Added, Front end Splash Created.

---
+++ app/src/main/java/uk/ac/tees/mad/w9519946/cravingsnearby/MainActivity.java
@@ -0,0 +1,15 @@
+package uk.ac.tees.mad.w9519946.cravingsnearby;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/uk/ac/tees/mad/w9519946/cravingsnearby/Screen_Splash.java
@@ -0,0 +1,28 @@
+package uk.ac.tees.mad.w9519946.cravingsnearby;
+
+import android.os.Bundle;
+import android.view.animation.Animation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Screen_Splash extends AppCompatActivity {
+
+
+    private static int SPLASH_SCREEN = 3000;
+
+    Animation blast_animation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen_splash);
+
+
+
+
+
+
+
+
+    }
+}
