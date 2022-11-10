4fb74006c63af153b67f61792f8e178db63a252f
Samyuk Yadav Banda <aronisam325@gmail.com>
2022-11-10 13:26:56 +0000

    Done with Front End XML Credential Screens

--- app/src/main/java/uk/ac/tees/mad/w9519946/cravingsnearby/Choice_Activity.java
+++ app/src/main/java/uk/ac/tees/mad/w9519946/cravingsnearby/Choice_Activity.java
@@ -7,12 +7,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Pair;
 import android.view.View;
+import android.view.WindowManager;
 
 public class Choice_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_choice);
         getSupportActionBar().hide();
 
--- app/src/main/java/uk/ac/tees/mad/w9519946/cravingsnearby/Screen_Settings_user.java
+++ app/src/main/java/uk/ac/tees/mad/w9519946/cravingsnearby/Screen_Settings_user.java
@@ -3,15 +3,13 @@ package uk.ac.tees.mad.w9519946.cravingsnearby;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.WindowManager;
 
-public class Screen_Login extends AppCompatActivity {
+public class Screen_Settings_user extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_screen_login);
+        setContentView(R.layout.activity_screen_settings_user);
         getSupportActionBar().hide();
     }
 }
