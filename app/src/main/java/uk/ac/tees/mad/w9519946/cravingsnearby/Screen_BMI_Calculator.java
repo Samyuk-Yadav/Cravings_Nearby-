053b2837a665ffcccc17f0b2fe027841980d3761
Samyuk Yadav Banda <aronisam325@gmail.com>
2022-12-12 15:47:41 +0000

    Working on BMI Backend Activity...

--- app/src/main/java/uk/ac/tees/mad/w9519946/cravingsnearby/Screen_BMI_Calculator.java
+++ app/src/main/java/uk/ac/tees/mad/w9519946/cravingsnearby/Screen_BMI_Calculator.java
@@ -1,14 +1,39 @@
 package uk.ac.tees.mad.w9519946.cravingsnearby;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Screen_BMI_Calculator extends AppCompatActivity {
 
     //Variables...
+    String user_type_ = "0";
+    String current_user_weight = "70";
+    String current_user_age = "24";
+    String prog_in_mi = "142";
+
+    int current_weight_default = 71;
+    int live_prog;
+    int current_age_default = 24;
+
+    RelativeLayout boy_gender;
+    RelativeLayout girl_gender;
+    TextView height_present_now;
+    TextView age_Number_txt;
+    ImageView weight_plus1;
+    SeekBar length_of_seekbar;
+    TextView weight_Number_txt;
+    ImageView age1_minus;
+    ImageView weight_minus1;
+    ImageView age1_plus;
     android.widget.Button bmi_calculator1_btn;
 
     @Override
@@ -17,15 +42,125 @@ public class Screen_BMI_Calculator extends AppCompatActivity {
         setContentView(R.layout.activity_screen_bmi_calculator);
         getSupportActionBar().hide();
 
-        //Hooks...
+        //Hooks...TypeCasting...
+        age_Number_txt = findViewById(R.id.age_Number_txt);
+        length_of_seekbar = findViewById(R.id.length_of_seekbar);
+        boy_gender = findViewById(R.id.boy_gender);
+        age1_minus = findViewById(R.id.age1_minus);
+        girl_gender = findViewById(R.id.girl_gender);
+        weight_minus1 = findViewById(R.id.weight_minus);
+        age1_plus = findViewById(R.id.age1_plus);
+        length_of_seekbar.setProgress(169);
+        weight_plus1 = findViewById(R.id.weight_plus);
+        length_of_seekbar.setMax(280);
+        height_present_now = findViewById(R.id.height_present_now);
+        weight_Number_txt  = findViewById(R.id.weight_Number_txt);
         bmi_calculator1_btn = findViewById(R.id.bmi_calculator1);
+
+        weight_plus1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                current_weight_default += 1;
+                current_user_weight = String.valueOf(current_weight_default);
+                weight_Number_txt.setText(current_user_weight);
+
+            }
+        });
+
+        age1_plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                current_age_default += 1;
+                current_user_age = String.valueOf(current_age_default);
+                age_Number_txt.setText(current_user_age);
+
+            }
+        });
+
+        girl_gender.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                girl_gender.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.focusing_gender_background));
+                boy_gender.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.not_focusing_gender_background));
+                user_type_ = "Female";
+            }
+        });
+
 
         bmi_calculator1_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent x = new Intent(Screen_BMI_Calculator.this, Screen_Final_BMI_Calculator.class);
-                startActivity(x);
+
+                if (user_type_.equals("0")){
+                    Toast.makeText(Screen_BMI_Calculator.this, "Select Gender First!!!", Toast.LENGTH_SHORT).show();
+                }else if (current_weight_default<0 || current_weight_default==0){
+                    Toast.makeText(Screen_BMI_Calculator.this, "Incorrect Weight!!!", Toast.LENGTH_SHORT).show();
+                }
+                else if(current_age_default<0 || current_age_default==0){
+                    Toast.makeText(Screen_BMI_Calculator.this, "Incorrect Age!!!", Toast.LENGTH_SHORT).show();
+                }
+                else if(prog_in_mi.equals("0")){
+                    Toast.makeText(Screen_BMI_Calculator.this, "Select Height First!!!", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Intent x = new Intent(Screen_BMI_Calculator.this, Screen_Final_BMI_Calculator.class);
+                    x.putExtra("height_of_user", prog_in_mi);
+                    x.putExtra("age_of_user", current_user_age);
+                    x.putExtra("weight_of_user", current_user_weight);
+                    x.putExtra("gender_of_user", user_type_);
+                    startActivity(x);
+                }
             }
         });
+
+        boy_gender.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boy_gender.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.focusing_gender_background));
+                girl_gender.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.not_focusing_gender_background));
+                user_type_ = "Male";
+            }
+        });
+
+        length_of_seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar sbar, int i, boolean b) {
+                live_prog = i;
+                prog_in_mi = String.valueOf(live_prog);
+                height_present_now.setText(prog_in_mi);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar sbar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar sbar) {
+
+            }
+        });
+
+        weight_minus1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                current_weight_default -= 1;
+                current_user_weight = String.valueOf(current_weight_default);
+                weight_Number_txt.setText(current_user_weight);
+
+            }
+        });
+
+        age1_minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                current_age_default -= 1;
+                current_user_age = String.valueOf(current_age_default);
+                age_Number_txt.setText(current_user_age);
+
+            }
+        });
+
+
     }
 }
--- app/src/main/java/uk/ac/tees/mad/w9519946/cravingsnearby/Screen_Final_BMI_Calculator.java
+++ app/src/main/java/uk/ac/tees/mad/w9519946/cravingsnearby/Screen_Final_BMI_Calculator.java
@@ -3,20 +3,53 @@ package uk.ac.tees.mad.w9519946.cravingsnearby;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 public class Screen_Final_BMI_Calculator extends AppCompatActivity {
 
     //Variables...
+    TextView gender_BMI_Value;
+    TextView cat_BMI_Value;
+    float floating_BMI_Value;
+    Intent xData;
+    String calculated_BMI_Value;
+    LottieAnimationView lottieAnimationView;
+    TextView display_BMI_Value;
     android.widget.Button bmi_calculator;
+
+    String user_Weight_Value;
+    String user_Height_Value;
+    RelativeLayout bg_relativeLayout;
+    float floating_weight_value;
+    float floating_height_value;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen_final_bmi_calculator);
         getSupportActionBar().hide();
+       // getSupportActionBar().setElevation(0);
+       // getSupportActionBar().setTitle(Html.fromHtml("<font color=\"white\"></font>"));
+       // getSupportActionBar().setTitle("Your BMI is Here!!!");
+       // ColorDrawable drawable_color = new ColorDrawable(Color.parseColor("#1E1D1ED"));
+       // getSupportActionBar().setBackgroundDrawable(drawable_color);
+
         //Hooks...
+        bg_relativeLayout = findViewById(R.id.layout_line);
+        xData = getIntent();
+        gender_BMI_Value = findViewById(R.id.gender_BMI_Value);
+        cat_BMI_Value = findViewById(R.id.cat_BMI_Value);
+        display_BMI_Value = findViewById(R.id.display_BMI_Value);
+        lottieAnimationView = findViewById(R.id.animated_image);
         bmi_calculator = findViewById(R.id.bmi_calculator);
 
 
@@ -28,6 +61,50 @@ public class Screen_Final_BMI_Calculator extends AppCompatActivity {
                 finish();
             }
         });
+
+
+        user_Height_Value = xData.getStringExtra("height_of_user");
+        floating_height_value  = Float.parseFloat(user_Height_Value);
+        floating_height_value = floating_height_value/100;
+        user_Weight_Value = xData.getStringExtra("weight_of_user");
+        floating_weight_value = Float.parseFloat(user_Weight_Value);
+        floating_BMI_Value = floating_weight_value/(floating_height_value*floating_height_value);
+        gender_BMI_Value.setText(xData.getStringExtra("gender_of_user"));
+
+
+        calculated_BMI_Value = Float.toString(floating_BMI_Value);
+        display_BMI_Value.setText(calculated_BMI_Value);
+
+        if (floating_BMI_Value<16.1){
+            cat_BMI_Value.setText("Under Weight");
+            bg_relativeLayout.setBackgroundColor(Color.MAGENTA);
+            lottieAnimationView.setAnimation(R.raw.cross);
+
+        }else if(floating_BMI_Value>18.5 && floating_BMI_Value<25){
+            cat_BMI_Value.setText("Normal");
+            bg_relativeLayout.setBackgroundColor(Color.GREEN);
+            lottieAnimationView.setAnimation(R.raw.ok);
+        }
+        else if(floating_BMI_Value>17.1 && floating_BMI_Value<18.5){
+            cat_BMI_Value.setText("Mild Thinness");
+            bg_relativeLayout.setBackgroundColor(Color.RED);
+            lottieAnimationView.setAnimation(R.raw.warn);
+
+        }else if (floating_BMI_Value>25 && floating_BMI_Value<29.5){
+            cat_BMI_Value.setText("Over Weight (Obesity Type 1)");
+            bg_relativeLayout.setBackgroundColor(Color.RED);
+            lottieAnimationView.setAnimation(R.raw.warn);
+        }
+        else if(floating_BMI_Value>16.1 && floating_BMI_Value<16.9){
+            cat_BMI_Value.setText("Moderate Weight");
+            bg_relativeLayout.setBackgroundColor(Color.RED);
+            lottieAnimationView.setAnimation(R.raw.warn);
+        }
+        else if(floating_BMI_Value>29.5 && floating_BMI_Value<35){
+           cat_BMI_Value.setText("Obese (Obesity Type 2)");
+           bg_relativeLayout.setBackgroundColor(Color.RED);
+           lottieAnimationView.setAnimation(R.raw.cross);
+        }
 
 
     }
